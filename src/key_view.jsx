e397c92eb70386c103b31050e683d7f3f2f1f6a9
Tom de Grunt <tom@degrunt.nl>
2015-11-01 22:18:16 +0100

    Allow rotate, offset, etc

--- src/key_view.jsx
+++ src/key_view.jsx
@@ -1,5 +1,12 @@
 export var KeyView = React.createClass({
   render: function() {
-    return <div className="key" data-key="38" style={{'top': this.props.y*54 +'px', 'left': this.props.x*54+'px'}}><div className="keytop"></div></div>;
+    var styles = {
+      'top': (this.props.voffset||0)+this.props.y*54 +'px',
+      'left': (this.props.hoffset||0)+this.props.x*54+'px',
+      'width': (this.props.width||1)*54+'px',
+      'transform': 'rotate('+(this.props.rotate||0)+'deg)',
+      'transform-origin': 'top left',
+    };
+    return <div className="key" style={styles}><div className="keytop"></div></div>;
   }
 });
--- src/layer_view.jsx
+++ src/layer_view.jsx
@@ -1,10 +1,13 @@
 import {KeyView} from 'key_view';
 
 export var LayerView = React.createClass({
+  getInitialState: function() {
+    return {keys: []};
+  },
   render: function() {
     var keys = this.props.keys.map(function (key) {
       return (
-        <KeyView x={key.x} y={key.y}/>
+        <KeyView x={key.x} y={key.y} width={key.width} voffset={key.voffset} hoffset={key.hoffset} rotate={key.rotate}/>
       );
     });
     return (
