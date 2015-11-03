510f03df6ccef47e36d73b7bbbd3e24d29fbb095
Tom de Grunt <tom@degrunt.nl>
2015-11-03 22:29:01 +0100

    Set keytop style automatically

--- src/key_view.jsx
+++ src/key_view.jsx
@@ -3,17 +3,22 @@ import {decodeHTML} from 'utils';
 export var KeyView = React.createClass({
   render: function() {
     var styles = {
-      'top': (this.props.data.voffset*54||0)+this.props.data.y*54 +'px',
-      'left': (this.props.data.hoffset*54||0)+this.props.data.x*54+'px',
-      'width': (this.props.data.width||1)*54+'px',
-      'height': (this.props.data.height||1)*54+'px',
-      'transform': 'rotate('+(this.props.data.rotate||0)+'deg)',
-      'transformOrigin': this.props.data.origin||'top left',
-      'backgroundColor': this.props.data.backgroundColor,
+      top: (this.props.data.voffset*54||0)+this.props.data.y*54,
+      left: (this.props.data.hoffset*54||0)+this.props.data.x*54,
+      width: (this.props.data.width||1)*54,
+      height: (this.props.data.height||1)*54,
+      transform: 'rotate('+(this.props.data.rotate||0)+'deg)',
+      transformOrigin: this.props.data.origin||'top left',
+      backgroundColor: this.props.data.backgroundColor
     };
+    var keyTopStyles = {
+      height: styles.height - 13,
+      width: styles.width - 8
+    };
+
     return (
       <div className='key' style={styles} onClick={this.handleClick}>
-        <div className="keytop">
+        <div className="keytop" style={keyTopStyles}>
           <div className="l l-tl">{decodeHTML(this.props.data.label||'')}</div>
         </div>
       </div>
