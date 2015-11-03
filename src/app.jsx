ee09e68bcdece307569e48b868189942df92331e
Tom de Grunt <tom@degrunt.nl>
2015-11-04 00:15:00 +0100

    Add panels, allow changing of most details

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,21 @@
 import {LayerView} from 'layer_view';
 import {LayerSelection} from 'layer_selection';
+import {LayoutPanel} from 'layout_panel';
+import {LayerPanel} from 'layer_panel';
+import {KeyPanel} from 'key_panel';
 
 export var App = React.createClass({
   getInitialState: function() {
     return {
+      layout: this.props.layout,
       selectedLayer: 0,
       selectedKey: null,
     };
+  },
+
+  addLayer: function() {
+    this.state.layout.layers.push({description: 'Untitled', keymap: []});
+    this.setState(this.state);
   },
 
   selectLayer: function(layer) {
@@ -14,17 +23,51 @@ export var App = React.createClass({
     this.setState(this.state);
   },
 
-  selectKey(key) {
+  selectKey: function(key) {
     this.state.selectedKey = this.state.selectedKey == key ? null : key;
     this.setState(this.state);
   },
 
+  layoutInfoChange: function(description) {
+    this.state.layout.description = description;
+    this.setState(this.state);
+  },
+
+  layerInfoChange: function(description) {
+    this.state.layout.layers[this.state.selectedLayer].description = description;
+    this.setState(this.state);
+  },
+
+  keyInfoChange: function(description, code, ltl) {
+    this.state.layout.layers[this.state.selectedLayer].keymap[this.state.selectedKey].description = description;
+    this.state.layout.layers[this.state.selectedLayer].keymap[this.state.selectedKey].code = code;
+    this.state.layout.layers[this.state.selectedLayer].keymap[this.state.selectedKey].label = ltl;
+    this.setState(this.state);
+  },
+
   render: function() {
-    var keymap = this.props.layout.layers[this.state.selectedLayer].keymap;
+    var selectedLayer = this.state.selectedLayer;
+    var selectedKey = this.state.selectedKey;
+    var keymap = this.state.layout.layers[selectedLayer].keymap;
+    var keyInfo = keymap[this.state.selectedKey];
     return (
       <div>
-        <LayerSelection layers={this.props.layout.layers} selectedLayer={this.state.selectedLayer} onSelectLayer={this.selectLayer}/>
-        <LayerView keys={this.props.keys} keymap={keymap} selectedKey={this.state.selectedKey} onSelectKey={this.selectKey}/>
+        <button onClick={this.addLayer}>Add layer</button>
+        <LayerSelection layers={this.state.layout.layers} selectedLayer={selectedLayer} onSelectLayer={this.selectLayer}/>
+        <LayerView keys={this.props.keys} keymap={keymap} selectedKey={selectedKey} onSelectKey={this.selectKey}/>
+        <div className="row">
+          <div className="col-sm-4">
+            <LayerPanel layer={this.state.layout.layers[selectedLayer]} selectedLayer={selectedLayer} onLayerInfoChange={this.layerInfoChange}/>
+          </div>
+          <div className="col-sm-4">
+            { selectedKey != null ? <KeyPanel keyInfo={keyInfo} selectedKey={selectedKey}
+                                                 onKeyInfoChange={this.keyInfoChange}/> : null
+            }
+          </div>
+          <div className="col-sm-4">
+            <LayoutPanel layout={this.state.layout} onLayoutInfoChange={this.layoutInfoChange}/>
+          </div>
+        </div>
       </div>
     );
   }
--- src/layer_view.jsx
+++ src/layer_view.jsx
@@ -6,10 +6,11 @@ export var LayerView = React.createClass({
     var styles = {
       display: 'block',
       position: 'relative',
+      height: 415,
     };
     var keys = this.props.keys.map(function (key, i) {
       var data = key;
-      data.label = self.props.keymap[i].label;
+      data.label = self.props.keymap[i] ? self.props.keymap[i].label : '';
       return (
         <KeyView data={data} key={i} keyIndex={i} selectedKey={self.props.selectedKey} onSelectKey={self.props.onSelectKey}/>
       );
